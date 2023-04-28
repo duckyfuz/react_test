73757a30769f0c3ceb2f5e23e705f36d71d55d16
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2023-04-28 20:12:35 +0800

    Finished Trails

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  let items = ["New York", "San Fransisco", "Singapore", "Tokyo"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          <strong>You pressed the button!</strong>
+          <p>Press x to dismiss the alert.</p>
+        </Alert>
+      )}
+
+      <ListGroup
+        items={items}
+        heading="Cties"
+        onSelectItem={handleSelectItem}
+      />
+
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
+        Create ALERT!
+      </Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
